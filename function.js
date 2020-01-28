61ecca5b2b3a059aa0da3454e872e1048603c2ab
lglasser87 <lizbeth.cazarez@gmail.com>
2020-01-28 15:45:10 -0800

    modified functions

--- function.js
+++ function.js
@@ -1,82 +1,82 @@
-function QuizQuestion(question, choices, correctAnswer){
-    this.question = question;
-    this.choices = choices;
-    this.correctAnswer = correctAnswer;
+function quizQuestion(question, choices, correctAnswer) {
+  this.question = question;
+  this.choices = choices;
+  this.correctAnswer = correctAnswer;
+}
+
+var allQuestions = [
+  new quizQuestion("What does CSS stand for?", ["Cascading Style Sheets", "Creative Style Sheets", "Computer Style Sheets", "Colorful Style Sheets"], 0),
+  new quizQuestion("How do you write and IF statement in JavaScript?", ["if i == 5 then", "if i = 5", "if (i == 5)", "if i == 5 then"], 2),
+  new quizQuestion("Which of the following jQuery method get the text contents of an element?", ["text()", "getText()", "getContent()", "none of the above"], 0),
+  new quizQuestion("Which sign does jQuery use asa shortcut for jQuery?", ["%", "?", "$", "&"], 2),
+  new quizQuestion("What scripting language is jQuery written in?", ["VBScript", "C#", "C++", "JavaScript"], 3),
+
+];
+
+var currentquestion = 0;
+var correctAnswers = 0;
+
+function setQuestions() {
+  $('#question').html(parseInt(currentquestion) + 1 + ". " + allQuestions[currentquestion].question);
+  var options = allQuestions[currentquestion].choices;
+  var formHtml = '';
+  for (var i = 0; i < options.length; i++) {
+    formHtml += '<div><input type="radio" name="option" value="' + i + '" class="options"><label for="option' + i + '">' + options[i] + '</label></div><br/>';
   }
-    
-  var allQuestions = [
-    new QuizQuestion("What does CSS stand for?",["Cascading Style Sheets", "Creative Style Sheets", "Computer Style Sheets", "Colorful Style Sheets"],0),
-    new QuizQuestion("How do you write and IF statement in JavaScript?",["if i == 5 then", "if i = 5", "if (i == 5)", "if i == 5 then"],2),
-    new QuizQuestion("Which of the following jQuery method get the text contents of an element?",[ "text()", "getText()", "getContent()", "none of the above"],0),
-    new QuizQuestion("Which sign does jQuery use asa shortcut for jQuery?",["%", "?", "$", "&"],2),
-    new QuizQuestion("What scripting language is jQuery written in?",["VBScript", "C#", "C++", "JavaScript"],3),
-     
-  ];
-  
-  var currentquestion = 0;
-  var correctAnswers = 0;
-  
-  function setupOptions() {
-    $('#question').html(parseInt(currentquestion) + 1 + ". " + allQuestions[currentquestion].question);
-    var options = allQuestions[currentquestion].choices;
-    var formHtml = '';
-    for (var i = 0; i < options.length; i++) {
-      formHtml += '<div><input type="radio" name="option" value="' + i + '" class="options"><label for="option' + i + '">' + options[i] + '</label></div><br/>';
-    }
-    $('#form').html(formHtml);
-    $(".options:eq(0)").prop('checked', true);
+  $('#form').html(formHtml);
+  $(".options:eq(0)").prop('checked', true);
+}
+
+function checkAnswer() {
+  if ($("input[name=option]:checked").val() == allQuestions[currentquestion].correctAnswer) {
+    correctAnswers++;
   }
-  
-  function checkAns() {
-    if ($("input[name=option]:checked").val() == allQuestions[currentquestion].correctAnswer) {
-      correctAnswers++;
-    }
-  }
-  
-  $(document).ready(function(){
-      
-    var $jumbotron = $(".jumbotron");
-    var $start = $("#start");
-    var $progressbar = $("#progressbar");
-    var $next = $("#next");
-    var $result = $("#result");
-    
-      $jumbotron.hide();
-      $start.click(function() {
-          $jumbotron.fadeIn();
-          $(this).hide();
-        });
-  
-      $(function() {
-          $progressbar.progressbar({
-              max: allQuestions.length-1,			
-              value: 0
-          });
-      });
-  
-      setupOptions();
-  
-      $next.click(function(){
-              event.preventDefault();
-              checkAns();
-              currentquestion++;
-              $(function() {
-                  $progressbar.progressbar({
-                        value: currentquestion
-                  });
-                });
-              if(currentquestion<allQuestions.length){
-                  setupOptions();
-                  if(currentquestion==allQuestions.length-1){
-                      $next.html("Submit");
-                      $next.click(function(){
-                          $jumbotron.hide();
-                          $result.html("You correctly answered " + correctAnswers + " out of " + currentquestion + " questions! ").hide();
-                          $result.fadeIn(1500);
-                      });
-  
-                  }
-                  
-              };
-      });	
+}
+
+$(document).ready(function () {
+
+  var $jumbotron = $(".jumbotron");
+  var $start = $("#start");
+  var $progressbar = $("#progressbar");
+  var $next = $("#next");
+  var $result = $("#result");
+
+  $jumbotron.hide();
+  $start.click(function () {
+    $jumbotron.fadeIn();
+    $(this).hide();
   });
+
+  $(function () {
+    $progressbar.progressbar({
+      max: allQuestions.length - 1,
+      value: 0
+    });
+  });
+
+  setQuestions();
+
+  $next.click(function () {
+    event.preventDefault();
+    checkAnswer();
+    currentquestion++;
+    $(function () {
+      $progressbar.progressbar({
+        value: currentquestion
+      });
+    });
+    if (currentquestion < allQuestions.length) {
+      setQuestions();
+      if (currentquestion == allQuestions.length - 1) {
+        $next.html("Submit");
+        $next.click(function () {
+          $jumbotron.hide();
+          $result.html("You correctly answered " + correctAnswers + " out of " + currentquestion + " questions! ").hide();
+          $result.fadeIn(1500);
+        });
+
+      }
+
+    };
+  });
+});
